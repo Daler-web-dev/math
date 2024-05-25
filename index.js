28d6757bdee403e620bebef3e5a17a12df6f4260
Daler <dsarifkulo021@gmail.com>
2024-05-25 17:55:58 +0500

    math explained

---
+++ index.js
@@ -0,0 +1,23 @@
+// number
+
+
+// let a = "20" // string
+// let b = "21" // string
+
+
+// console.log(
+    // округление
+    // Math.floor(num) // округляет всегда вниз
+    // Math.ceil(num) // округляет всегда в верх 
+    // Math.round(num) // округляет логически
+    // округление
+
+    // Math.max(1,2,3,num, numtwo)
+    // Math.min(1,2,3,num, numtwo, -1)
+
+    // a + b 
+    // a
+    // Number(a) // "20" -> 20 // string -> number
+    // String(a) // 20 -> "20" // number -> string
+    // +a // "20" -> 20 // string -> number
+// );
